8f5d7a0f8a163085e7778662a583e5b94d043fc0
Don Denton <don@happycollision.com>
2017-11-29 18:12:54 -0600

    Touch screen support (#1)
    * swap click event for touch event
    
    * update lib and dist
    
    * add some smarts about what type of input to use
    
    * update lib and dist
    
    * change code
    
    * update lib and dist
    
    * move allowOnly out of class
    
    * update lib and dist
    
    * add main for ease of use
    
    * pure function syntax

--- src/KeyboardButton.js
+++ src/KeyboardButton.js
@@ -1,6 +1,8 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
+let allowOnly = 0;
+
 export default class KeyboardButton extends PureComponent {
 	static propTypes = {
 		value: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.node.isRequired]),
@@ -16,14 +18,34 @@ export default class KeyboardButton extends PureComponent {
 		isDisabled: false,
 	};
 
-	handleClick = () => this.props.onClick(this.props.value)
+	handleValidInput = () => {
+		console.log('valid input detected');
+		if (!this.props.isDisabled) {
+			this.props.onClick(this.props.value)
+		}
+	}
+
+	handleClickAttempt = (e) => {
+		console.log('click attempted');
+		if (allowOnly === 2) return;
+		if (allowOnly === 0) allowOnly = 1;
+		if (allowOnly === 1) this.handleValidInput();
+	}
+
+	handleTouchAttempt = (e) => {
+		console.log('touch attempted');
+		if (allowOnly === 1) return;
+		if (allowOnly === 0) allowOnly = 2;
+		if (allowOnly === 2) this.handleValidInput();
+	}
 
 	render() {
 		return (
 			<button
 				type="button"
 				className={`keyboard-button ${this.props.classes}`}
-				onClick={this.props.isDisabled ? null : this.handleClick}
+				onMouseUp={this.handleClickAttempt}
+				onTouchStart={this.handleTouchAttempt}
 				autoFocus={this.props.autofocus}
 				disabled={this.props.isDisabled}
 			>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Keyboard from './Keyboard';
+import KeyboardButton from './KeyboardButton';
+import LatinLayout from './layouts/LatinLayout';
+import CyrillicLayout from './layouts/CyrillicLayout';
+import NumericLayout from './layouts/NumericLayout';
+
+export default Keyboard
+export {Keyboard}
+export {KeyboardButton}
+export {LatinLayout}
+export {CyrillicLayout}
+export {NumericLayout}
